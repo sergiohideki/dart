6832ca185ab0f019eafda3dd63f7940b5ebf5593
Sergio Hideki Ono <41549046+sergiohideki@users.noreply.github.com>
2020-04-22 15:23:02 -0300

    desafio for

---
+++ controle/desafio_for.dart
@@ -0,0 +1,15 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+// Use o laço FOR, mas não pode controlar o laço usando valor numérico!
+
+main() {
+  
+  for (var valor = '#'; valor != '#######'; valor += '#') {
+    print(valor);
+  }
+}
