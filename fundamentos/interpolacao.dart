8a59f0e6cd1e99bbef37643741b4060cd6661668
Sergio Hideki Ono <41549046+sergiohideki@users.noreply.github.com>
2020-04-22 12:37:04 -0300

    interpolação

---
+++ fundamentos/interpolacao.dart
@@ -0,0 +1,13 @@
+main(){
+  String nome = 'João';
+  String status = 'aprovado';
+  double nota = 9.2;
+
+  String frase1 = nome + ' está ' + status + ' porque tirou nota ' + nota.toString() + '!';
+  print(frase1);
+
+  String frase2 = '$nome está $status porque  tirou nota $nota!'; // interpolação
+  print(frase2);
+
+  print('1 + 1 = ${1 + 1}');
+}
