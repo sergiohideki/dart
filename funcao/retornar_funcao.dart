c4cb8b6ac33bc43940420a23f3e77bb8b7fc207e
Sergio Hideki Ono <41549046+sergiohideki@users.noreply.github.com>
2020-04-25 10:20:06 -0300

    função que retorna função

---
+++ funcao/retornar_funcao.dart
@@ -0,0 +1,17 @@
+int Function(int) somaParcial(int a){
+  int c = 0;
+
+  return (int b){
+    return a + b + c;
+  };
+}
+
+main(){
+  print(somaParcial(2)(10));
+
+  var somaComAeB = somaParcial(10);
+
+  print(somaComAeB(3));
+  print(somaComAeB(7));
+  print(somaComAeB(20));
+}
